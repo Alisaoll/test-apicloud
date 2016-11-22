0545b73f0e7b096fe0ee030d59525705ecf032b7
2013-20161009KJ\Administrator <1076756788@qq.com>
2016-11-22 17:41:25 +0800

    no message

---
+++ config.js
@@ -0,0 +1,8 @@
+export const DEBUG = true
+export const HEADER_HEIGHT = 45
+export const FOOTER_HEIGHT = 50
+export const ANDROID_STATUSBAR_HEIGHT = 25
+export const IOS_STATUSBAR_HEIGHT = 20
+export const WIN_ANIMATION_DURATION = 300
+export const REQUEST_TIMEOUT = 30
+export const CDN = ''
